7b6b0e877e84a77b400849fe539423a8f7cd1467
Dixen Cheng <dixentw@gmail.com>
2018-07-21 23:14:25 +0900

    test deploy

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.get('*', function(req, res){
     res.send('what???', 404);
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
 });
